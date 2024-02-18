4d8f42e9f705b6916337f17c3b6b1f5a51a2ded6
Ashok S <59574169+ashok3612@users.noreply.github.com>
2024-02-18 17:25:53 +0530

    update 6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React On VCET 5
+          Learn React On VCET 6
         </a>
       </header>
     </div>
