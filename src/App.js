a1f7ba96f5112e900ac341ba10b40d40c2f77e05
Boobalanselvaraj <142650588+Boobalanselvaraj@users.noreply.github.com>
2024-02-19 10:24:14 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 //gokul
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React On VCET 7
-        </a>
+        <>
+        <h2>LOGIN PAGE</h2>
+        <p>VELALAR COLLEGE OF ENGINEERING AND TECHNOLOGY</p>
+        </>
       </header>
     </div>
   );
