3580316e1093a78bf7647ce6a642d9b77b56434a
Gokulsarav <gokul.slj@gmail.com>
2024-03-08 12:58:18 +0530

    about problem

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Home } from "./pages/Home";
 import { Courses } from "./pages/Courses";
+import About from './About';
 
-
-import CourseList from './CourseList';
-import LatestCourseSlider from './LatestCourseSlider';
-import HomeTop from './HomeTop';
-import Footer from './Footer';
 function App() {
   return (
     <>
@@ -19,6 +15,7 @@ function App() {
           <Route path="/" Component={Home} />
           <Route path="/contact" Component={Contact} />
           <Route path="/Courses" Component={Courses} />
+          <Route path="/About" Component={About} />
         </Routes>
         
         {/* </div> */}
