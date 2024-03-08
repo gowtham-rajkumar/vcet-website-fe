35058634feeea1e1bed3264da338f47ae9938b9a
gowtham-rajkumar <gowtham.jlrs@gmail.com>
2024-03-08 13:05:42 +0530

    Created Data.js and  Course.js to simplified the LatestCourseSlider.js

---
+++ src/Data.js
@@ -0,0 +1,198 @@
+export const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 1024 },
+        items: 3,
+        slidesToSlide: 1
+    },
+    desktop: {
+        breakpoint: { max: 1024, min: 800 },
+        items: 3,
+        slidesToSlide: 1
+    },
+    tablet: {
+        breakpoint: { max: 800, min: 500 },
+        items: 2,
+        slidesToSlide: 1
+    },
+    mobile: {
+        breakpoint: { max: 500, min: 0 },
+        items: 1
+    }
+};
+
+export const UG_CourseHeadingBox = {
+    heading:"UG Programmes", 
+    description:"Empower your trajectory with our renowned UG programmes"
+};
+
+export const PG_CourseHeadingBox = {
+    heading:"PG Programmes", 
+    description:"Embark on a journey of excellence with our top tier PG programmes"
+};
+
+export const UG_CourseData = [
+    {
+        id: 1,
+        img: "https://source.unsplash.com/random/500x250/?animal?1",
+        program: "B.E",
+        dept: "BME",
+        description: "Innovating health solutions for tomorrow's medical advancements."
+    },
+
+    {
+        id: 2,
+        img: "https://source.unsplash.com/random/500x250/?animal?2",
+        program: "B.E",
+        dept: "Civil",
+        description: "Building the world with precision and sustainability in mind."
+    },
+
+    {
+        id: 3,
+        img: "https://source.unsplash.com/random/500x250/?animal?3",
+        program: "B.E",
+        dept: "CSE",
+        description: "Transforming ideas into cutting-edge technology for a digital future."
+    },
+
+    {
+        id: 4,
+        img: "https://source.unsplash.com/random/500x250/?animal?4",
+        program: "B.E",
+        dept: "EEE",
+        description: "Powering technological innovations shaping our electrifying world."
+    },
+
+    {
+        id: 5,
+        img: "https://source.unsplash.com/random/500x250/?animal?5",
+        program: "B.E",
+        dept: "ECE",
+        description: "Empowering global connectivity through integrated electronic systems."
+    },
+
+    {
+        id: 6,
+        img: "https://source.unsplash.com/random/500x250/?animal?6",
+        program: "B.E",
+        dept: "Mech",
+        description: "Engineering excellence driving industries forward with innovative solutions."
+    },
+
+    {
+        id: 7,
+        img: "https://source.unsplash.com/random/500x250/?animal?7",
+        program: "B.E",
+        dept: "MDE",
+        description: "Pioneering medical technology for healthier tomorrows."
+    },
+
+    {
+        id: 8,
+        img: "https://source.unsplash.com/random/500x250/?animal?8",
+        program: "B.Tech",
+        dept: "AIDS",
+        description: "Unleashing the potential of data with AI for transformative insights."
+    },
+
+    {
+        id: 9,
+        img: "https://source.unsplash.com/random/500x250/?animal?9",
+        program: "B.Tech",
+        dept: "AIML",
+        description: "Harnessing AI to create intelligent systems for a smarter world."
+    },
+
+    {
+        id: 10,
+        img: "https://source.unsplash.com/random/500x250/?animal?10",
+        program: "B.Tech",
+        dept: "IT",
+        description: "Empowering the digital era with cutting-edge IT solutions and innovations."
+    }
+
+];
+
+export const PG_CourseData = [
+    {
+        id: 1,
+        img: "https://source.unsplash.com/random/500x250/?animal?1",
+        program: "B.E",
+        dept: "BME",
+        description: "Innovating health solutions for tomorrow's medical advancements."
+    },
+
+    {
+        id: 2,
+        img: "https://source.unsplash.com/random/500x250/?animal?2",
+        program: "B.E",
+        dept: "Civil",
+        description: "Building the world with precision and sustainability in mind."
+    },
+
+    {
+        id: 3,
+        img: "https://source.unsplash.com/random/500x250/?animal?3",
+        program: "B.E",
+        dept: "CSE",
+        description: "Transforming ideas into cutting-edge technology for a digital future."
+    },
+
+    {
+        id: 4,
+        img: "https://source.unsplash.com/random/500x250/?animal?4",
+        program: "B.E",
+        dept: "EEE",
+        description: "Powering technological innovations shaping our electrifying world."
+    },
+
+    {
+        id: 5,
+        img: "https://source.unsplash.com/random/500x250/?animal?5",
+        program: "B.E",
+        dept: "ECE",
+        description: "Empowering global connectivity through integrated electronic systems."
+    },
+
+    {
+        id: 6,
+        img: "https://source.unsplash.com/random/500x250/?animal?6",
+        program: "B.E",
+        dept: "Mech",
+        description: "Engineering excellence driving industries forward with innovative solutions."
+    },
+
+    {
+        id: 7,
+        img: "https://source.unsplash.com/random/500x250/?animal?7",
+        program: "B.E",
+        dept: "MDE",
+        description: "Pioneering medical technology for healthier tomorrows."
+    },
+
+    {
+        id: 8,
+        img: "https://source.unsplash.com/random/500x250/?animal?8",
+        program: "B.Tech",
+        dept: "AIDS",
+        description: "Unleashing the potential of data with AI for transformative insights."
+    },
+
+    {
+        id: 9,
+        img: "https://source.unsplash.com/random/500x250/?animal?9",
+        program: "B.Tech",
+        dept: "AIML",
+        description: "Harnessing AI to create intelligent systems for a smarter world."
+    },
+
+    {
+        id: 10,
+        img: "https://source.unsplash.com/random/500x250/?animal?10",
+        program: "B.Tech",
+        dept: "IT",
+        description: "Empowering the digital era with cutting-edge IT solutions and innovations."
+    }
+
+];
