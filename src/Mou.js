2712ca65a31af71d8ec08ea6baf2ab753f8be61b
gowtham-rajkumar <gowtham.jlrs@gmail.com>
2024-03-12 14:02:30 +0530

    Updated Data.js

---
+++ src/Mou.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Mou = (props) => {
+  return (
+    <div className='MuoContainer'>
+      <img 
+        src={props.img}
+        alt="MOU_Image"
+      />
+    </div>
+  )
+}
+
+export default Mou;
